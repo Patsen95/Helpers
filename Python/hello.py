62c935703fb7dcc4bb92f670c15cc4e869f314b1
Patryk Sienkiewicz <patsen995@gmail.com>
2022-10-17 09:41:42 +0200

    UI widgets testing code

--- Python/hello.py
+++ Python/hello.py
@@ -6,6 +6,7 @@ from tkinter import messagebox
 
 from p95.statusbar import *
 from p95.tableview import *
+from p95.entrytable import *
 
 
 class Application:
@@ -14,48 +15,58 @@ class Application:
 
 		self.tk = Tk()
 		self.tk.title("UI widgets Test")
-		self.tk.geometry("800x600")
+		self.tk.geometry("900x600")
 		self.window = ttk.Frame(self.tk)
+		# self.window.grid()
 
-		sbar = StatusBar(self.tk)
-		sbar.addLabel("first", "label1")
-		sbar.addLabel("sec", "label2", 10)
-		sbar.addLabel("third", "trzeci!")
-		sbar.addLabel("czw", None)
+		# sbar = StatusBar(self.tk)
+		# sbar.addLabel("t1", "Test Label #1")
+		# sbar.addLabel("t2", "Test Label #2", 10)
+		# sbar.addSeparator("sep", 20)
+		# sbar.addLabel("t3", "Test Label #3", 10)
+
+		# tv = TableView(self.tk, 150, 50, 50, 50, 50, 50)
+		# tv.setPos(850, 550)
+
+		# self.window.grid()
+
+		# names = ("This is col1", "col2", "col3", "trololoddddddddddddddd")
+		names = ("This is col1", "oiawdoiawd")
+		self.et = EntryTable(self.tk, 100, 100, 3, names)
+
+		btn1 = Button(self.window, text="Click me", command=self.click)
+		btn1.grid(column=0, row=0)
 
 
-		sbar.labelOffset("third", 50)
-		
-		sbar.labelOffset("czw", 100)
-		kupa = 3.14159
-		sbar.setValue("czw", kupa)
+		# self.et.deleteRow(2)
 
 
 
-		# btn = Button(self.window, text="Click me", command=self.onClick)
-		# btn.pack()
+		# btn1 = Button(self.window, text="Click me")
+		# btn2 = Button(self.window, text="Click me")
+		# btn1.grid(column=0, row=0)
+		# btn2.grid(column=1, row=1)
 
-		# tv = TableView(self.tk, 20, 100, 50, 50)
-		
-		
-		# tv.place(relx=0)
+		# print(self.window.grid_size())
+		# print(self.window.grid_slaves())
 
 		self.window.pack()
+
+	def click(self):
+		self.et.deleteRow(2)
 	
-	def appMainLoop(self):
+	def mainLoop(self):
 		self.tk.mainloop()
 
-	def closeApp(self):
+	def close(self):
 		self.tk.destroy
-		
-# button1 = Button(frm, height=5, width=10, text="WYPIERDALAJ", command=root.destroy).grid(column=0, row=1)
-# button2 = Button(frm, text="KLIK", command=Dupa).grid(column=1, row=1)
 
 
 ################################################
+
 def main():
 	appInstance = Application()	
-	appInstance.appMainLoop()
+	appInstance.mainLoop()
 
 
 if __name__ == "__main__":
